7e28640105976b15a3e706477de19f9dabb61982
giovanni <ii2090082@gmail.com>
2022-02-25 20:53:05 +0200

    adds akka-streaming stuff

--- src/main/scala/hi/Main.scala
+++ src/main/scala/hi/Main.scala
@@ -3,14 +3,18 @@ package hi
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model._
+import akka.stream.OverflowStrategy
+import akka.stream.scaladsl.{Flow, Sink, Source}
 
+import scala.concurrent.duration.DurationInt
 import scala.concurrent.{ExecutionContextExecutor, Future}
-import scala.io.Source
+import scala.util.{Failure, Success}
 
 object Main {
   val REFRESH_TIME = 10 * 60 * 1000
 
-  var file = fetchUrls
+  val source = scala.io.Source.fromFile("urls.txt")
+  var file = source.getLines().toList
 
   var lastUpdate = System.currentTimeMillis()
 
@@ -18,35 +22,44 @@ object Main {
     implicit val system: ActorSystem = ActorSystem()
     implicit val executionContext: ExecutionContextExecutor = system.dispatcher
 
-    var i = 0
-    while (true) {
-      println(s"iteration $i")
-
-      if (System.currentTimeMillis() - lastUpdate > REFRESH_TIME) {
-        file = fetchUrls
-        println(s"list updated. size=${file.size}")
-      }
-
-      file.foreach {
-        url =>
-          println(s"$url -- start with love")
-          val responseFuture: Future[HttpResponse] = Http().singleRequest(HttpRequest(uri = url))
-
-          responseFuture.map {
-            case response@HttpResponse(StatusCodes.OK, _, _, _) =>
-              println("response OK")
-            case err =>
-              sys.error(s"server is not responding: $err")
-          }
-      }
-
-      Thread.sleep(50)
-      i += 1
+    if (System.currentTimeMillis() - lastUpdate > REFRESH_TIME) {
+      file = fetchUrls
+      println(s"list updated. size=${file.size}")
     }
+    val (queue, aggregationSource) = akka.stream.scaladsl.Source
+      .queue[String](file.size, OverflowStrategy.backpressure)
+      .throttle(50, 1.second)
+      .wireTap(Flow.fromFunction[String, Future[HttpResponse]](url => {
+        println(s"$url -- start with love")
+        Http().singleRequest(HttpRequest(uri = url))
+      })
+        .async
+        .to(Sink
+          .foreach(
+            responseFuture => {
+              responseFuture.onComplete { result => {
+                result match {
+                  case Success(value) => println(s"Response status: ${value.status}")
+                  case Failure(exception) => println(s"Error happened: ${exception.getClass.getName}")
+                }
+              }
+              }
+            }
+          )))
+      .preMaterialize()
+
+    Source(file)
+      .via(
+        Flow[String]
+          .merge(aggregationSource)
+          .map(p => queue.offer(p))
+      )
+      .run()
   }
 
+
   private def fetchUrls = {
-    val source = Source.fromURL("https://raw.githubusercontent.com/david-l-books/storage/main/urls.txt")
+    val source = scala.io.Source.fromURL("https://raw.githubusercontent.com/david-l-books/storage/main/urls.txt")
     try {
       source.getLines.toList
     } finally {
